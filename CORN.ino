6e97d2116cfdfde80631982dadb9d4954d11651c
Nicola Cimmino <nicola.cimmino@gmail.com>
2020-12-20 12:31:05 +0100

    Refactor to use DCServices

--- CORN.ino
+++ CORN.ino
@@ -17,8 +17,9 @@
 //    along with this program.  If not, see http://www.gnu.org/licenses/.
 //
 
+
 #include "RomanDisplay.h"
-#include "NRF24L01RadioDriver.h"
+#include <DCServices.h>
 #include "uRTCLib.h"
 
 #define LED_PIN 7
@@ -27,6 +28,8 @@ uRTCLib rtc(0x68);
 RomanDisplay display;
 NRF24L01RadioDriver *radio;
 
+DCServices *dcServices;
+
 void setup()
 {
     Wire.begin();
@@ -34,11 +37,7 @@ void setup()
     display.clearDisplay();
     display.show();
 
-    radio = new NRF24L01RadioDriver(2);
-    radio->setRXExtendedPreamble(643234);
-    radio->setTXExtendedPreamble(643234);
-    radio->setTXPower(3);
-    radio->setRFChannel(32);
+    dcServices = new DCServices(DC_RADIO_NRF24_V2, &rtc);
 
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, HIGH);
@@ -46,32 +45,12 @@ void setup()
 
 void loop()
 {
-    char buffer[64];
-    uint8_t dataLen = 64;
-    if (radio->receive(buffer, &dataLen, 1000))
-    {       
-        Serial.println(buffer); 
-        char *token = strtok(buffer, ",");
-        if (strcmp(*token, "^T"))
-        {
-            token = strtok(NULL, ",");
-            uint8_t h = atoi(token);
-            token = strtok(NULL, ",");
-            uint8_t m = atoi(token);
-            token = strtok(NULL, ",");
-            uint8_t s = atoi(token);
-
-            Serial.println(h);
-            Serial.println(m);
-            Serial.println(s);
-            rtc.set(s, m, h, 0, 0, 0, 0);          
-
-            digitalWrite(LED_PIN, LOW);
-            delay(200);
-            digitalWrite(LED_PIN, HIGH);
-        }
-
-        memset(buffer, 0, 64);
+    bool syncOK = dcServices->syncRTCToTimeBroadcast();
+    if (syncOK)
+    {
+        digitalWrite(LED_PIN, LOW);
+        delay(100);
+        digitalWrite(LED_PIN, HIGH);
     }
 
     rtc.refresh();
